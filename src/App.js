3fee5356f191c22b193437b775b70216824250e9
Sergii Bogush <sergbogush@gmail.com>
2021-07-27 09:26:43 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Statistics from "./components/Statistics";
 import FeedbackOptions from "./components/FeedbackOptions";
 import Section from "./components/Section";
@@ -6,49 +6,105 @@ import Header from "./components/Header";
 import { options } from "./data/options";
 import Container from "./components/Container";
 
-class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
-  onLeaveFeedback = ({ target }) => {
+export default function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  // const onLeaveFeedback = ({ target }) => {
+  //   const { mark } = target.dataset;
+  //   this.setState((prevState) => ({ [mark]: prevState[mark] + 1 }));
+  // };
+
+  const onLeaveFeedback = ({ target }) => {
     const { mark } = target.dataset;
-    this.setState((prevState) => ({ [mark]: prevState[mark] + 1 }));
+    console.log(mark);
+    switch (mark) {
+      case "good":
+        setGood((prevState) => prevState + 1);
+        break;
+      case "neutral":
+        setNeutral((prevState) => prevState + 1);
+        break;
+      case "bad":
+        setBad((prevState) => prevState + 1);
+        break;
+      default:
+        return;
+    }
   };
-  countTotalFeedback = () => {
-    return this.state.good + this.state.neutral + this.state.bad;
+
+  const countTotalFeedback = () => {
+    return good + neutral + bad;
   };
 
-  countPositiveFeedbackPercentage() {
-    return Math.round((this.state.good / this.countTotalFeedback()) * 100);
-  }
+  const countPositiveFeedbackPercentage = () => {
+    return Math.round((good / countTotalFeedback()) * 100);
+  };
+  const total = countTotalFeedback();
+  const percentage = countPositiveFeedbackPercentage();
 
-  render() {
-    const total = this.countTotalFeedback();
-    const percentage = this.countPositiveFeedbackPercentage();
-
-    return (
-      <Container>
-        <Header />
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={options}
-            onLeaveFeedback={this.onLeaveFeedback}
-          />
-        </Section>
-        <Section title="Statistic">
-          <Statistics
-            good={this.state.good}
-            neutral={this.state.neutral}
-            bad={this.state.bad}
-            total={total}
-            percentage={percentage}
-          />
-        </Section>
-      </Container>
-    );
-  }
+  return (
+    <Container>
+      <Header />
+      <Section title="Please leave feedback">
+        <FeedbackOptions options={options} onLeaveFeedback={onLeaveFeedback} />
+      </Section>
+      <Section title="Statistic">
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={total}
+          percentage={percentage}
+        />
+      </Section>
+    </Container>
+  );
 }
+// class OldApp extends Component {
+//   state = {
+//     good: 0,
+//     neutral: 0,
+//     bad: 0,
+//   };
+//   onLeaveFeedback = ({ target }) => {
+//   const { mark } = target.dataset;
+//   this.setState((prevState) => ({ [mark]: prevState[mark] + 1 }));
+// }
+//   countTotalFeedback = () => {
+//     return this.state.good + this.state.neutral + this.state.bad;
+//   };
 
-export default App;
+//   countPositiveFeedbackPercentage() {
+//     return Math.round((this.state.good / this.countTotalFeedback()) * 100);
+//   }
+
+//   render() {
+//     const total = this.countTotalFeedback();
+//     const percentage = this.countPositiveFeedbackPercentage();
+
+//     return (
+//       <Container>
+//         <Header />
+//         <Section title="Please leave feedback">
+//           <FeedbackOptions
+//             options={options}
+//             onLeaveFeedback={this.onLeaveFeedback}
+//           />
+//         </Section>
+//         <Section title="Statistic">
+//           <Statistics
+//             good={this.state.good}
+//             neutral={this.state.neutral}
+//             bad={this.state.bad}
+//             total={total}
+//             percentage={percentage}
+//           />
+//         </Section>
+//       </Container>
+//     );
+//   }
+// }
+
+// export default App;
